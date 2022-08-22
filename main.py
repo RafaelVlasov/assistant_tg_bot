ca45b5e79285cc869a9eb11b23addfc418091cf1
RafaelVlasov <rafael.vlasov@gmail.com>
2022-08-22 16:44:02 +0300

    add func save, del and view links

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from telebot import types  # импортируем модуль для созд
 bot = telebot.TeleBot('2010273573:AAF-chsJjiWh6nlMlk1gR0cYNKHDe7104as')  # присваиваем токен бота из ТГ
 
 
-@bot.message_handler(commands=['help'])   # обработчик для считывания команд пользователя
+@bot.message_handler(commands=['help'])  # обработчик для считывания команд пользователя
 def helper(message):
     help_message = '<b>/help</b> - вывести справку по функциональностям\n<b>/view_words</b> - показать список ' \
                    'сохранённых слов\n<b>/view_dict</b> - показать собранный it-словарь\n<b>/to_do_list</b> - ' \
                    'показать список дел\n<b>/view_links</b> - показать сохранённые ссылки\n<b>add_word \"слово\"' \
                    '</b> - добавить слово во временный массив чтобы изучить его значение в будущем\n<b>add_dict \"' \
                    'слово - значение\"</b> - добавить слово и его определение в словарь\n<b>add_todo \"описание ' \
-                   'задачи\"</b> - добавить задачу в список дел\n<b>del_word \"слово\"</b> - удалить слово из ' \
-                   'временного массива\n<b>del_dict \"слово\"</b> - удалить слово и его описание из словаря\n' \
-                   '<b>del_todo \"номер задачи\"</b> - удалить задачу под указанным номером из списка дел\n' \
-                   '<b>del_link \"номер ссылки\"</b> - удалить ссылку под указанным номером '
+                   'задачи\"</b> - добавить задачу в список дел\n<b>add_link</b> - сохранить ссылку\n<b>del_word' \
+                   ' \"слово\"</b> - удалить слово из временного массива\n<b>del_dict \"слово\"</b> - удалить слово' \
+                   ' и его описание из словаря\n<b>del_todo \"номер задачи\"</b> - удалить задачу под указанным ' \
+                   'номером из списка дел\n<b>del_link \"номер ссылки\"</b> - удалить ссылку под указанным номером '
     bot.send_message(message.chat.id, help_message, parse_mode='html')
 
 
@@ -29,7 +29,8 @@ def start(message):
     view_words_button = types.KeyboardButton('/view_words')
     view_dict_button = types.KeyboardButton('/view_dict')
     to_do_list_button = types.KeyboardButton('/to_do_list')
-    markup.add(help_button, start_button, view_words_button, view_dict_button, to_do_list_button)
+    view_links_button = types.KeyboardButton('/view_links')
+    markup.add(help_button, start_button, view_words_button, view_dict_button, to_do_list_button, view_links_button)
     # через message.chat.id обращаемся к нашему с ботом чату, отправляем сообщение, формат отправки parse_mode
     # используем html чтобы иметь возможность управлять видом текста с помощью тагов
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
@@ -65,6 +66,16 @@ def to_do_list(message):
         bot.send_message(message.chat.id, '\n'.join(todo_list), parse_mode='html')
 
 
+#  просмотр списка ссылок
+@bot.message_handler(commands=['view_links'])
+def view_links(message):
+    send_mess = f'<b>{message.from_user.first_name}</b>, вот список сохранённых ссылок:'
+    bot.send_message(message.chat.id, send_mess, parse_mode='html')
+    with open("links.txt", 'r', encoding='utf-8') as file:
+        links = [line.strip() for line in file.readlines()]
+        bot.send_message(message.chat.id, '\n'.join(links), parse_mode='html')
+
+
 #  добавление и удаление данных
 @bot.message_handler()
 def add_and_del_data(message):
@@ -79,6 +90,13 @@ def add_and_del_data(message):
         bot.send_message(message.chat.id, send_mess, parse_mode='html')
         with open("it_dict.txt", 'a', encoding='utf-8') as file:
             file.write(message.text[9:] + '\n')
+    elif 'add_link' in message.text:
+        send_mess = f'Отлично <b>{message.from_user.first_name}</b>! Я сохранил новую ссылку в список'
+        bot.send_message(message.chat.id, send_mess, parse_mode='html')
+        with open("links.txt", 'r', encoding='utf-8') as file:
+            links = [line.strip() for line in file.readlines()]
+        with open("links.txt", 'a', encoding='utf-8') as file:
+            file.write(str(len(links) + 1) + ") " + message.text[9:] + '\n')
     elif 'add_todo' in message.text:
         send_mess = f'Отлично <b>{message.from_user.first_name}</b>! Я сохранил новую задачу в список дел'
         bot.send_message(message.chat.id, send_mess, parse_mode='html')
@@ -103,7 +121,6 @@ def add_and_del_data(message):
         else:
             send_mess = f'<b>{message.from_user.first_name}</b>, я не нашёл слово <b>{message.text[9:]}</b> в списке'
             bot.send_message(message.chat.id, send_mess, parse_mode='html')
-
     elif 'del_dict' in message.text:
         with open("it_dict.txt", 'r', encoding='utf-8') as file:
             temp_list_dict = [line.strip() for line in file.readlines()]
@@ -140,6 +157,28 @@ def add_and_del_data(message):
                     for word in temp_todo_list:
                         file.write(str(todo_num) + word[1:] + '\n')
                         todo_num += 1
+                break
+        if count > 0:
+            send_mess = f'<b>{message.from_user.first_name}</b>, я удалил задачу <b>{task[3:]}</b> из списка дел!'
+            bot.send_message(message.chat.id, send_mess, parse_mode='html')
+        else:
+            send_mess = f'<b>{message.from_user.first_name}</b>, я не нашёл задачу под этим номером в твоём списке дел'
+            bot.send_message(message.chat.id, send_mess, parse_mode='html')
+    elif 'del_link' in message.text:
+        with open("links.txt", 'r', encoding='utf-8') as file:
+            temp_links_list = [line.strip() for line in file.readlines()]
+        count = 0
+        task = ''
+        for i in range(len(temp_links_list)):
+            if message.text[9:] + ')' in temp_links_list[i]:
+                task = temp_links_list[i]
+                del temp_links_list[i]
+                count += 1
+                with open("links.txt", 'w', encoding='utf-8') as file:
+                    link_num = 1
+                    for word in temp_links_list:
+                        file.write(str(link_num) + word[1:] + '\n')
+                        link_num += 1
                 break
         if count > 0:
             send_mess = f'<b>{message.from_user.first_name}</b>, я удалил задачу <b>{task[3:]}</b> из списка дел!'
